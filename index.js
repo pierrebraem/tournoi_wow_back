7c55d1c17374aabdd9d7a0c99eabbc3f2a1df038
Pierre Braem <pierrebraem@orange.fr>
2025-02-07 15:03:37 +0100

    config: Connexion à la base de données

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const express = require('express')
-require('dotenv').config()
+const express = require('express');
+require('dotenv').config();
+const db = require('./db');
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
+app.get('/', async (req, res) => {
+    try {
+        const result = await db.query('SELECT * FROM roles');
+        res.json(result.rows);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
 });
+
+app.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const result = await db.query('SELECT * FROM roles WHERE id = $1', [id]);
+        res.json(result.rows);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+})
 
 app.listen(port, () => {
     console.log(`App running on ${port}`)
