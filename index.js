be2a589a9efa2997937cdf2a3eb1453229289455
Pierre Braem <pierrebraem@orange.fr>
2025-02-07 16:52:34 +0100

    feat: CRUD pour les personnages

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
-const db = require('./db');
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', async (req, res) => {
-    try {
-        const result = await db.query('SELECT * FROM roles');
-        res.json(result.rows);
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-    }
-});
+app.use(bodyParser.json());
 
-app.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-        const result = await db.query('SELECT * FROM roles WHERE id = $1', [id]);
-        res.json(result.rows);
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-    }
-})
+const charactersRouter = require('./routes/characters');
+
+app.use('/characters', charactersRouter);
 
 app.listen(port, () => {
-    console.log(`App running on ${port}`)
+    console.log(`App running on ${port}`);
 });
