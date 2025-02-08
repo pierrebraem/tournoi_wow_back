3568d3ec88dbcfa44c83765de78888c6bab0e591
Pierre Braem <pierrebraem@orange.fr>
2025-02-08 10:03:13 +0100

    feat: Mise en place de cors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'] 
+}))
 
 const charactersRouter = require('./routes/characters');
 
