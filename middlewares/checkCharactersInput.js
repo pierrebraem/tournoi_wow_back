fcb27bf9f0ad812aedc517b23053f1b4d8ef52c1
Pierre Braem <pierrebraem@orange.fr>
2025-02-09 12:12:07 +0100

    refacto: change 'filed' to 'field'

--- middlewares/checkCharactersInput.js
+++ middlewares/checkCharactersInput.js
@@ -2,37 +2,37 @@ const checkCharactersInput = (req, res, next) => {
     const body = req.body;
 
     if(body.name == undefined){
-        res.status(400).send({ "message": "The filed 'name' is required" });
+        res.status(400).send({ "message": "The field 'name' is required" });
         return;
     }
 
     if(body.class_id == undefined){
-        res.status(400).send({ "message": "The filed 'class_id' is required" });
+        res.status(400).send({ "message": "The field 'class_id' is required" });
         return;
     }
 
     if(body.role_id == undefined){
-        res.status(400).send({ "message": "The filed 'role_id' is required" });
+        res.status(400).send({ "message": "The field 'role_id' is required" });
         return;
     }
 
     if(body.ilvl == undefined){
-        res.status(400).send({ "message": "The filed 'ilvl' is required" });
+        res.status(400).send({ "message": "The field 'ilvl' is required" });
         return;
     }
 
     if(body.rio == undefined){
-        res.status(400).send({ "message": "The filed 'rio' is required" });
+        res.status(400).send({ "message": "The field 'rio' is required" });
         return;
     }
 
     if(body.ilvl < 0 || body.ilvl > 645){
-        res.status(400).send({ "message": "The filed 'ilvl' must be between 0 and 645" });
+        res.status(400).send({ "message": "The field 'ilvl' must be between 0 and 645" });
         return;
     }
 
     if(body.rio < 0 || body.rio > 4500){
-        res.status(400).send({ "message": "The filed 'rio' must be between 0 and 4500" });
+        res.status(400).send({ "message": "The field 'rio' must be between 0 and 4500" });
         return;
     }
 
