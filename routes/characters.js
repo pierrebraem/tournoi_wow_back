39b7228fc18550a13906cd980172a0e79ca530ad
Pierre Braem <pierrebraem@orange.fr>
2025-02-10 20:12:17 +0100

    refacto: suppresio results.rowCount

--- routes/characters.js
+++ routes/characters.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
     const id = req.params.id;
 
     try{
-        const result = await db.query("SELECT characters.id, characters.name, class.label class, roles.label role, characters.ilvl, characters.rio FROM characters INNER JOIN class ON characters.class_id = class.id INNER JOIN roles ON characters.role_id = roles.id WHERE characters.id = $1", [id]);
+        const result = await db.query("SELECT characters.id, characters.name, class.id class_id, class.label class, roles.id roles_id, roles.label role, characters.ilvl, characters.rio FROM characters INNER JOIN class ON characters.class_id = class.id INNER JOIN roles ON characters.role_id = roles.id WHERE characters.id = $1", [id]);
         res.json(result.rows);
     }
     catch(err){
@@ -34,10 +34,7 @@ router.post('/', checkCharactersInput, async (req, res) => {
     const body = req.body;
 
     try{
-        const result = await db.query('INSERT INTO characters (name, class_id, role_id, ilvl, rio) VALUES ($1, $2, $3, $4, $5)', [body.name, body.class_id, body.role_id, body.ilvl, body.rio]);
-        if(result.rowCount == 0){
-            throw new Error("Character has not been added");
-        }
+        await db.query('INSERT INTO characters (name, class_id, role_id, ilvl, rio) VALUES ($1, $2, $3, $4, $5)', [body.name, body.class_id, body.role_id, body.ilvl, body.rio]);
         res.status(201).send({ "message": "Created" });
     }
     catch(err){
@@ -52,10 +49,7 @@ router.put("/:id", checkCharactersInput, async (req, res) => {
     const body = req.body;
 
     try{
-        const result = await db.query("UPDATE characters SET name = $2, class_id = $3, role_id = $4, ilvl = $5, rio = $6 WHERE id = $1", [id, body.name, body.class_id, body.role_id, body.ilvl, body.rio]);
-        if(result.rowCount == 0){
-            throw new Error("Character has not been updated");
-        }
+        await db.query("UPDATE characters SET name = $2, class_id = $3, role_id = $4, ilvl = $5, rio = $6 WHERE id = $1", [id, body.name, body.class_id, body.role_id, body.ilvl, body.rio]);
         res.status(200).send({ "message": "Updated" });
 
     }
@@ -69,10 +63,8 @@ router.delete("/:id", async (req, res) => {
     const id = req.params.id;
 
     try{
-        const result = await db.query("DELETE FROM characters WHERE id = $1", [id]);
-        if(result.rowCount == 0){
-            throw new Error("Character has not been deleted");
-        }
+        await db.query("DELETE FROM compose WHERE characters_id = $1", [id]);
+        await db.query("DELETE FROM characters WHERE id = $1", [id]);
         res.status(200).send({ "message": "Deleted" });
     }
     catch(err){
